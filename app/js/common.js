e7d0c6015852bdbfeabea90670a7272477b9278f
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-07-17 14:12:12 +0300

    dropdown-menu js done

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,36 @@
 $(function() {
+	//dropdown menu
+	var menu = $('.left-menu');
+
+	menu.on('click', function(e) {
+		var target = e.target;
+		e.preventDefault();
+		while (target !== this) {
+			if (target.tagName === 'A') {
+				break;
+			}
+			target = target.parentNode;
+		}
+		if (!target.nextElementSibling) return;
+		var dropdown = target.nextElementSibling;
+		
+		$(target).focusout(function() {
+			$(dropdown).slideUp('fast');
+		})
+		$(dropdown).slideToggle('fast');
+	});
 	
+	//second dropdown menu hover
+	menu.find('li ul li').hover(
+	  function () {
+	    $('ul', this).stop().show();
+	  },
+	  function () {
+	    $('ul', this).stop().hide();
+	  }
+	);
+
+	//second dropdown menu height
+	var h = $('.dropdown-menu').height();
+	$('.second-dropdown-menu').height(h);
 });
