f8f12d57a73f7499ce74c0086a0d69b9a595c2f6
Oleksii Teplenko <pytnik.tov@gmail.com>
2016-07-19 12:38:19 +0300

    form validation done

--- app/js/common.js
+++ app/js/common.js
@@ -95,4 +95,50 @@ $(function() {
 	});
 
 
+	// Form validate
+	
+	var form 			= $("#myForm"),
+		formName 		= $("#myForm #name"),
+		formEmail 		= $("#myForm #email"),
+		formPassword 	= $("#myForm #password");
+
+	var regName 		= /^[a-z0-9_-]{3,16}$/,
+		regEmail 		= /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/,
+		regPassword 	= /^[a-z0-9_-]{6,18}$/;
+
+
+	form.submit(function(e) {
+
+		if (!regName.test(formName.val())) {
+		    formName.focus();
+		    formName.closest('.input').css('box-shadow', '0 0 5px 5px #f36');
+		    e.preventDefault();
+		    return;
+		} else {
+			formName.closest('.input').css('box-shadow', '');
+		}
+
+	 	if (!regEmail.test(formEmail.val())) {
+		    formEmail.focus();
+		    formEmail.closest('.input').css('box-shadow', '0 0 5px 5px #f36');
+		    e.preventDefault();
+		    return;
+		} else {
+			formEmail.closest('.input').css('box-shadow', '');
+		}
+
+		if (!regPassword.test(formPassword.val())) {
+		    formPassword.focus();
+		    formPassword.closest('.input').css('box-shadow', '0 0 5px 5px #f36');
+		    e.preventDefault();
+		    return;
+		} else {
+			formPassword.closest('.input').css('box-shadow', '');
+		}
+	  
+		formName.val('');
+		formEmail.val('');
+		formPassword.val('');
+	});
+
 });
